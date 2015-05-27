9932b2b26a3b5ce669eb9b5a357eebaebc0c5aa4
johan vlaar <johan.vlaar.1994@gmail.com>
2015-05-27 11:46:54 +0200

    reserveren

--- reserveren.php
+++ reserveren.php
@@ -12,72 +12,90 @@
   			<div class="row">
 	  				<div class="col-md-7">
 						<form role="form">
-							<div class="form-group">
-								<label for="naam">Uw naam: *</label>
-								<input type="text" class="form-control" id="naam" required>
+							<div class="col-xs-12 col-sm-6 col-md-6">
+								<div class="form-group">
+									<label for="naam">Uw voornaam: *</label>
+									<input type="text" class="form-control" id="naam" required>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-6 col-md-6">
+								<div class="form-group">
+									<label for="naam">Uw achternaam: *</label>
+									<input type="text" class="form-control" id="naam" required>
+								</div>
+							</div>
+							<div class="col-xs-12 col-md-12 col-md-12">
+								<div class="form-group">
+									<label for="email">Uw E-Mail: *</label>
+									<input type="email" class="form-control" id="email" required>
+								</div>
+							</div>
+							<div class="col-xs-12 col-md-12 col-md-12">
+								<div class="form-group">
+									<label for="tel">Uw telefoonnummer: *</label>
+									<input type="tel" onkeypress="return isNumberKey(event)" class="form-control" id="tel" required>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-6 col-md-6">
+								<div class="form-group">
+									<label for="pers">Hoeveel personen? *</label>
+									<input type="text" class="form-control" id="pers" required>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-6 col-md-6">
+								<div class="form-group">
+									<label for="tijd">Voorkeurstijd? *</label>
+									<input type="text" class="form-control" id="tijd" required>
+								</div>
+							</div>
+							<div class="col-xs-12 col-md-12 col-md-12">
+								<div class="form-group">
+									<label for="datum">Datum *</label>
+									<input type="date" class="form-control" id="datum" required>
+								</div>
+							</div>
+							<div class="col-xs-12 col-md-12 col-md-12">
+								<div class="form-group">
+									<label for="comment">Extra informatie</label>
+									<textarea class="form-control" rows="5" id="comment"></textarea>
+								</div>
+							</div>
+								
+							<div class="col-xs-12 col-md-12 col-md-12">
+								<p>Vergeet niet de verplichte velden met een (*) in te vullen</p>
+							</div>
+							
+							<div class="col-md-12">
+								<button type="submit" class="btn btn-default">Verstuur reservering</button><br><br>
 							</div>
 
-							<div class="form-group">
-								<label for="tel">Uw telefoonnummer: *</label>
-								<input type="tel" onkeypress="return isNumberKey(event)" class="form-control" id="tel" required>
-							</div>
-
-							<div class="form-group">
-								<label for="pers">Hoeveel personen? *</label>
-								<input type="text" class="form-control" id="pers" required>
-							</div>
-
-							<div class="form-group">
-								<label for="email">Uw E-Mail: *</label>
-								<input type="email" class="form-control" id="email" required>
-							</div>
-
-							<div class="form-group">
-								<label for="tijd">Voorkeurstijd? *</label>
-								<input type="text" class="form-control" id="tijd" required>
-							</div>
-
-							<div class="form-group">
-								<label for="datum">Datum *</label>
-								<input type="date" class="form-control" id="datum" required>
-							</div>
-
-							<div class="form-group">
-								<label for="comment">Extra informatie</label>
-								<textarea class="form-control" rows="5" id="comment"></textarea>
-							</div>
-
-							<p>Vergeet niet de verplichte velden met een (*) in te vullen</p> <br>
-
-							<button type="submit" class="btn btn-default">Verstuur reservering</button><br><br>
 						</form>
      				</div>
 					<div class="col-md-5" id="informatie">
-						<p>
-							<strong>Café Restaurant Het Witte Paard</strong><br>
-							Hoofdstraat 217<br>
-							9828 PB Oostwold<br>
-							T: 050 - 5515 282<br>
-							M: <a href="mailto:reserveren@caferestauranthetwittepaard.nl">reserveren@caferestauranthetwittepaard.nl</a><br><br>
-						</p>
-						<p><strong>Openingstijden</strong></p>
-						 <p style="float: left;">
-							 Maandag:&nbsp<br>
-							Dinsdag:&nbsp<br>
-							Woensdag:&nbsp<br>
-							Donderdag:&nbsp<br>
-							Vrijdag:&nbsp<br>
-							Zaterdag:&nbsp<br>
-							Zondag:&nbsp<br></p>
-						<p style="float: left;">
-							Gesloten<br>
-							Gesloten<br>
-							12:00<br>
-							12:00<br>
-							12:00<br>
-							12:00<br>
-							15:00<br></p>
-     				</div>
+							<div class="col-xs-12 col-sm-6 col-md-12">
+								<h1>Adress gegevens</h1>
+									<div class="col-xs-12 col-sm-12 col-md-6">
+										Hoofdstraat 217<br>
+										9828 PB Oostwold<br>
+										T: 050 - 5515 282<br>
+										M: <a href="mailto:reserveren@caferestauranthetwittepaard.nl">reserveren@caferestauranthetwittepaard.nl</a><br><br>
+									</div>
+							</div>
+							<div class="col-xs-12 col-sm-6 col-md-12">
+								<h1>Openingstijden</h1>
+							
+								<div class="col-xs-12 col-sm-12 col-md-6">
+									Maandag: Gesloten<br>
+									Dinsdag: Gesloten<br>
+									Woensdag: 12:00 uur<br>
+									Donderdag: 12:00 uur<br>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-6">
+									Vrijdag: 12:00 uur<br>
+									Zaterdag: 12:00 uur<br>
+									Zondag: 15:00 uur<br>
+								</div>
+							</div>
    			 </div>
 		</div>
     </div>
