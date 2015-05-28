621b9897cbe0f5450b3f9db0d38a8932ccde96da
Martin Vinke <martin.vinke.mv@gmail.com>
2015-05-28 20:47:08 +0200

    Nog wat kleine aanpassingen aan de footer gedaan

--- resources/footer.php
+++ resources/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <div id="footer-holder" class="container-fluid">
 
-         <div id="footer-left" class="col-md-3" style="border: 1px solid black">
+         <div id="footer-left" class="col-md-3">
              <h6><b>Openingstijden</b></h6>
              <br>
              <p>
@@ -10,9 +10,10 @@
                  woensdag t/m zaterdag: vanaf 12.00 uur
                  zondag: vanaf 15:00..............................
              </p>
+             <div id="borderstyle"></div>
 
          </div>
-         <div id="footer-center" class="col-md-3" style="border: 1px solid black">
+         <div id="footer-center" class="col-md-3">
              <p><b>Contactgegevens</b></p>
              <p>
                  Restaurant Het Witte Paard<br>
@@ -24,17 +25,17 @@
 
                      <a href="#" class="svg webicon facebook">Like us on Facebook</a>
          </div>
-         <div id="footer-right" class="col-md-3" style="border: 1px solid black">
+         <div id="footer-right" class="col-md-3">
                  <h6><b>Handige linkjes</b></h6>
                  <a href="#">Reserveren</a><br>
                  <a href="#">Contact</a><br>
                  <a href="#">Algemene voorwaarden</a><br>
                  <a href="#">Cookies</a>
          </div>
-         <div id="footer-right" class="col-md-3" style="border: 1px solid black">RECHTS</div>
+         <div id="footer-rightimg" class="col-md-3" ><img class="img-responsive" src="img/locatie.jpg" style="height: 200px; width: 300px;" </div>
 
     </div>
-    <div class="container">
+    <div class="row container-fluid">
         <div id="copyrightholder" class="col-md-12">
             <span>© 2015 Copyright - Restaurant het Witte Paard Oostwold</span>
         </div>
