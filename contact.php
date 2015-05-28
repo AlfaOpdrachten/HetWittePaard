84ac6119c73b96ad00e70c88b63a1bb09e8caf13
johan vlaar <johan.vlaar.1994@gmail.com>
2015-05-28 10:06:31 +0200

    idea en ignore file

---
+++ contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<!-- De scripts -->
+<?php include 'resources/head.php'; ?>
+
+<body>
+<?php include 'resources/header.php'; ?>
+
+    <div class="content container ">
+    	<div class="container-fluid">
+  			<div class="row">
+
+            </div>
+        </div>
+    </div>
+
+    
+    
+
+<?php include 'resources/footer.php'; ?>
+
+</body>
+
+
+</html>
