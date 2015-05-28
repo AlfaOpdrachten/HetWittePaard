cfc74ff93bc3ee086df7aaee7ac8eae777a74a8b
Martin Vinke <martin.vinke.mv@gmail.com>
2015-05-28 10:13:09 +0200

    Plaatjes voor webicons en footer aangepast

--- resources/footer.php
+++ resources/footer.php
@@ -1,6 +1,40 @@
+<footer>
+    <div id="footer-holder" class="container">
 
-<div class="container">
-<div class="row">
-    <div class="col-lg-10">Footer</div>
-</div>  
+         <div id="footer-left" class="col-lg-3" style="border: 1px solid black">
+             <h6><b>Openingstijden</b></h6>
+             <p>
+                 maandag en dinsdag: Gesloten.............
+                 woensdag t/m zaterdag: vanaf 12.00 uur
+                 zondag: vanaf 15:00..............................
+             </p>
+
+         </div>
+         <div id="footer-center" class="col-lg-3" style="border: 1px solid black">
+             <p><b>Contactgegevens</b></p>
+             <p>
+                 Restaurant Het Witte Paard<br>
+                 Hoofdstraat 217<br>
+                 9828 PB Oostwold<br>
+                 T: 050 – 5515 282<br>
+                 M: info@caferestauranthetwittepaard.nl
+             </p>
+
+                     <a href="#" class="svg webicon facebook">Like us on Facebook</a>
+         </div>
+         <div id="footer-right" class="col-lg-3" style="border: 1px solid black">
+                 <h6><b>Handige linkjes</b></h6>
+                 <a href="#">Reserveren</a><br>
+                 <a href="#">Contact</a><br>
+                 <a href="#">Algemene voorwaarden</a><br>
+                 <a href="#">Cookies</a>
+         </div>
+         <div id="footer-right" class="col-lg-3" style="border: 1px solid black">RECHTS</div>
+
     </div>
+    <div class="container">
+        <div id="copyrightholder" class="col-lg-12">
+            <span>© 2015 Copyright - Restaurant het Witte Paard Oostwold</span>
+        </div>
+    </div>
+</footer>
