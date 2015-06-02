0f5cc371c6ad2d93700658f7176918e04534def5
JohJohan <johan.vlaar.1994@gmail.com>
2015-06-02 13:59:13 +0200

    menu

---
+++ kaart/dessert.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="nl">
+<!-- De scripts -->
+<?php include '../resources/head.php'; ?>
+
+<body>
+<?php include '../resources/header.php'; ?>
+
+<div class="content container ">
+    <div class="container-fluid" style="padding-left: 0;">
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/HetWittePaard/index.php'?>>Home</a></li>
+                <li><a href=<?php echo 'http://'.$_SERVER['SERVER_NAME'].'/HetWittePaard/kaart/index.php'?>>Kaart</a></li>
+                <li class="active">Dessert</li>
+            </ol>
+        </div>
+        <div class="col-md-4">
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, quia, adipisci. Voluptatem vel, nemo officiis nulla tempora magni ad numquam dicta recusandae. Fuga doloribus dolorum, fugiat ipsa accusantium illo delectus? 
+        </div>
+        <div class="col-md-4">
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, quia, adipisci. Voluptatem vel, nemo officiis nulla tempora magni ad numquam dicta recusandae. Fuga doloribus dolorum, fugiat ipsa accusantium illo delectus? 
+        </div>
+        <div class="col-md-4">
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, quia, adipisci. Voluptatem vel, nemo officiis nulla tempora magni ad numquam dicta recusandae. Fuga doloribus dolorum, fugiat ipsa accusantium illo delectus? 
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php include '../resources/footer.php'; ?>
+
+</body>
+
+
+</html>
