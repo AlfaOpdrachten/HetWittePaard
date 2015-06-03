e842034379f2d1e4e63f143ced893979c723c55c
Merlase <merlase2@gmail.com>
2015-06-03 10:58:34 +0200

    Home
    Home

--- index.php
+++ index.php
@@ -11,24 +11,39 @@
         <div class="row">
             <div class="col-md-8">
                 <div class="col-md-12" style="border: 1px solid black; margin-top: 10px; box-shadow: 0px 0px 5px 0px black;">
-                    <h1>Welkom</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloremque doloribus explicabo labore quasi ullam, ut! A, alias at atque est excepturi, fugiat harum possimus quibusdam ratione saepe tempore totam.</p>
+                    <h1>Over ons</h1>
+                    <p style="font-size: 15px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloremque doloribus explicabo labore quasi ullam, ut! A, alias at atque est excepturi, fugiat harum possimus quibusdam ratione saepe tempore totam.</p>
                 </div>
-                <div class="col-md-12" style="border: 1px solid black; margin-top: 10px; box-shadow: 0px 0px 5px 0px black;">
-                    <h1>Welkom</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem illum nesciunt quasi tempora tenetur. Eum incidunt laboriosam libero magnam nam neque odio quaerat quibusdam reiciendis veritatis! Incidunt porro quas sit!</p>
+                <div class="col-md-12" style="padding: 0px;">
+                    <div class="col-md-6" style="margin-top: 10px; padding: 0px; padding-right: 10px;">
+                        <div class="thumbnail" style="background-color: beige;">
+                            <img src="img/foodheader (1).jpg" alt="contact">
+                            <div class="caption">
+                                <p><a class="btn btn-lg" style="color: black; font-size: 35px;" href="/HetWittePaard/reserveren.php" role="button">Menu</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6" style="margin-top: 10px; padding: 0px; padding-left: 10px;">
+                        <div class="thumbnail" style="background-color: beige;">
+                            <img src="img/foodheader (1).jpg" alt="reserveren">
+                            <div class="caption">
+                                <p><a class="btn btn-lg" style="color: black; font-size: 35px;" href="/HetWittePaard/reserveren.php" role="button">Reserveren</a></p>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-md-4" style="border: 1px solid black; margin-bottom: 5px; margin-top: 10px; box-shadow: 0px 0px 5px 0px black;">
                 <div class="col-md-12">
-                    <h1>Week Aanbiedingen</h1>
+                    <h1 style="font-size: 30px">Week Aanbiedingen</h1>
                 </div>
                 <div class="col-md-12">
                     <div class="col-md-10">
                         <p>Steak van de grill</p>
                     </div>
                     <div class="col-md-2">
-                        <p>prijs</p>
+                        <p>&euro;14.99</p>
                     </div>
                     <div class="col-md-10">
                         <p>Met een heerlijke huisgemaakte zigeunersaus</p>
@@ -43,7 +58,7 @@
                         <p>Steak van de grill</p>
                     </div>
                     <div class="col-md-2">
-                        <p>prijs</p>
+                        <p>&euro;14.99</p>
                     </div>
                     <div class="col-md-10">
                         <p>Met een heerlijke huisgemaakte zigeunersaus</p>
@@ -58,7 +73,7 @@
                         <p>Steak van de grill</p>
                     </div>
                     <div class="col-md-2">
-                        <p>prijs</p>
+                        <p>&euro;14.99</p>
                     </div>
                     <div class="col-md-10">
                         <p>Met een heerlijke huisgemaakte zigeunersaus</p>
@@ -73,21 +88,21 @@
                         <p>Steak van de grill</p>
                     </div>
                     <div class="col-md-2">
-                        <p>prijs</p>
+                        <p>&euro;14.99</p>
                     </div>
                     <div class="col-md-10">
-                        <p>Met een heerlijke huisgemaakte zigeunersaus</p>
+                        <p>Met een heerlijke huisgemaakte zigeunersaus.</p>
 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
 
-<?php include 'resources/footer.php'; ?>
+    <?php include 'resources/footer.php'; ?>
 
 </body>
 
