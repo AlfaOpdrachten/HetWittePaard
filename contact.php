cb38cb153a1ab94a5d295c450750f9994221f0ba
stefwinter <stefwinter1995@gmail.com>
2015-06-09 13:26:47 +0200

    contactpagina

--- contact.php
+++ contact.php
@@ -10,6 +10,35 @@
     <div class="content container ">
     	<div class="container-fluid">
   			<div class="row">
+                    <div class="col-md-6">
+                <IMG SRC="img/restaurant.jpg" ALT="restaurant" WIDTH="500" HEIGHT="300">
+                </div>
+                <div class="col-md-5" id="informatie" style="margin-bottom: 100px;">
+                    <div class="col-xs-12 col-sm-6 col-md-12">
+                        <h1>Adres gegevens</h1>
+                        <div class="col-xs-12 col-sm-12 col-md-6">
+                            Hoofdstraat 217<br>
+                            9828 PB Oostwold<br>
+                            T: 050 - 5515 282<br>
+                            M: <a href="mailto:reserveren@caferestauranthetwittepaard.nl">reserveren@caferestauranthetwittepaard.nl</a><br><br>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-12">
+                        <h1>Openingstijden</h1>
+
+                        <div class="col-xs-12 col-sm-12 col-md-6">
+                            Maandag: Gesloten<br>
+                            Dinsdag: Gesloten<br>
+                            Woensdag: 12:00 uur<br>
+                            Donderdag: 12:00 uur<br>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-6">
+                            Vrijdag: 12:00 uur<br>
+                            Zaterdag: 12:00 uur<br>
+                            Zondag: 15:00 uur<br>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
