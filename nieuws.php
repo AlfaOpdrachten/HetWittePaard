e801fe7f7ad311d240fcf7f5be415c43de735c27
Martin Vinke <martin.vinke.mv@gmail.com>
2015-06-10 10:50:42 +0200

    Bezig geweest met de nieuws pagina

--- nieuws.php
+++ nieuws.php
@@ -2,11 +2,35 @@
 <html lang="nl">
 <!-- De scripts -->
 <?php include 'resources/head.php'; ?>
-
+<style>
+    div {
+        border: solid black 1px;
+    }
+</style>
 <body>
 <?php include 'resources/header.php'; ?>
 
+<div class="content container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="col-md-10">
+                    <h1>TEST</h1>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="col-md-10">
+                    <h1>TEST</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 <?php include 'resources/footer.php'; ?>
+</div>
+
 
 </body>
 
