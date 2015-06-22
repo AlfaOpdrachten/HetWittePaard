0fccff221cf2409fe7d3aea2c1bf6b03c189b2cd
JohJohan <johan.vlaar.1994@gmail.com>
2015-06-22 11:00:11 +0200

    index padding gefixed/mobiel

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
 <body>
 <?php include 'resources/header.php'; ?>
+
 <div class="container">
     <div class="row" style="padding-left: 15px; padding-right: 15px; margin-top: 15px;">
         <ol class="breadcrumb">
@@ -12,41 +13,39 @@
         </ol>
     </div>
 </div>
-<div class="content container ">
-    <div class="container-fluid" style="padding-left: 0;">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="col-md-12" style="border: 1px solid black; margin-top: 10px; box-shadow: 0px 0px 5px 0px black; background-color: bisque;">
-                    <h1>Over ons</h1>
-                    <p style="font-size: 15px;"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam cum omnis rerum! Accusamus, ad, dicta ea ipsum iusto laudantium minus modi molestiae neque, pariatur quas recusandae suscipit tenetur ullam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloremque doloribus explicabo labore quasi ullam, ut! A, alias at atque est excepturi, fugiat harum possimus quibusdam ratione saepe tempore totam.</p>
-                </div>
-                <div class="col-md-12" style="padding: 0px;">
-                    <div class="col-md-6" style="margin-top: 10px; padding: 0px; padding-right: 10px;">
-                        <a href="/HetWittePaard/kaart/index.php" style="padding-bottom: 20px;">
-                            <div class="thumbnail" style="background-color: bisque; border: 1px solid black; box-shadow: 0px 0px 5px 0px black;">
-                                <img src="img/menu1.jpg" alt="Menu">
-                                <div class="caption">
-                                    <p><a class="btn btn-link" style="color: black; font-size: 35px;" href="/HetWittePaard/kaart/index.php" role="button">Bekijk menu</a></p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6" style="margin-top: 10px; padding: 0px; padding-left: 10px; ">
-                        <a href="/HetWittePaard/reserveren.php">
-                            <div class="thumbnail" style="background-color: bisque; border: 1px solid black; box-shadow: 0px 0px 5px 0px black;">
-                                <img src="img/reserveren.jpg" alt="Reserveren">
-                                <div class="caption">
-                                    <p><a class="btn btn-link" style="color: black; font-size: 35px;" href="/HetWittePaard/reserveren.php" role="button">Reserveren</a></p>
-                        </a>
+<div class="content container index">
+    <div class="col-xs-12 col-md-8" style="padding-left:0px;">
+        <div class="col-md-12" style="border: 1px solid black; box-shadow: 0px 0px 5px 0px black; background-color: bisque;">
+            <h1>Over ons</h1>
+            <p style="font-size: 15px;"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam cum omnis rerum! Accusamus, ad, dicta ea ipsum iusto laudantium minus modi molestiae neque, pariatur quas recusandae suscipit tenetur ullam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloremque doloribus explicabo labore quasi ullam, ut! A, alias at atque est excepturi, fugiat harum possimus quibusdam ratione saepe tempore totam.</p>
+        </div>
+        <div class="col-xs-6 col-md-6" style="margin-top: 10px; padding: 0px;">
+            <a href="/HetWittePaard/kaart/index.php" style="padding-bottom: 20px;">
+                <div class="thumbnail" style="background-color: bisque; border: 1px solid black; box-shadow: 0px 0px 5px 0px black;">
+                    <img src="img/menu1.jpg" alt="Menu">
+                    <div class="caption">
+                        <p><a class="btn btn-link" style="color: black; font-size: 35px;" href="/HetWittePaard/kaart/index.php" role="button">Bekijk menu</a></p>
                     </div>
                 </div>
-            </div>
+            </a>
+        </div>
+        <div class="col-xs-6 col-md-6" style="margin-top: 10px; padding: 0px; ">
+            <a href="/HetWittePaard/reserveren.php">
+                <div class="thumbnail" style="background-color: bisque; border: 1px solid black; box-shadow: 0px 0px 5px 0px black;">
+                    <img src="img/reserveren.jpg" alt="Reserveren">
+                    <div class="caption">
+                        <p><a class="btn btn-link" style="color: black; font-size: 35px;" href="/HetWittePaard/reserveren.php" role="button">Reserveren</a></p>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
-    <div class="col-md-4" style="border: 1px solid black; margin-bottom: 50px; margin-top: 10px; box-shadow: 0px 0px 5px 0px black;">
+    <div class="col-xs-12 col-md-4" style="border: 1px solid black; margin-bottom: 50px; box-shadow: 0px 0px 5px 0px black;">
+        
         <div class="col-md-12">
             <h1 style="font-size: 30px; text-align: center;">Nieuws</h1>
         </div>
+
         <div class="col-md-12">
             <div class="col-md-7">
                 <p style="font-size: 20px;">Titel news</p>
