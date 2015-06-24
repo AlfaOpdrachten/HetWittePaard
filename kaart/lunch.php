86e09dd4e747c92dc0759e707e2ca4610c1fdb41
JohJohan <johan.vlaar.1994@gmail.com>
2015-06-24 13:48:38 +0200

    menu lunch ingeladen via cms

--- kaart/lunch.php
+++ kaart/lunch.php
@@ -23,182 +23,167 @@
 </div>
 <div class="content container " style="background-color: #FFDCAA;  box-shadow: 0px 0px 10px 0px gray; border: solid black 1px;">
     <div class="container-fluid" style="padding-left: 0;">
+                <?php
+
+                $data = file_get_contents('http://www.caferestauranthetwittepaard.nl/CMS/menu/?kaart=1');
+                $data = json_decode($data,1);
+
+                // echo '<pre>';var_dump($data);echo '</pre>';
+                // echo $data[$test]["Categorie"];
+
+               
+                ?>
        <div class="menuOverzicht">
             <div class="col-md-6">
-                <div class="col-xs-12 col-md-12">
-                    <h2>Soepen</h2>
-                </div>
-                    <div class="col-xs-8 col-md-8">
-                        <h3>Groninger mosterdsoep met gebakken spekjes<h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>6.50</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                        <h3>Champignonsoep<h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>4.50</p>
-                    </div>
-                <div class="col-xs-12 col-md-12">
-                    <h2>Voorgerechten</h2>
-                </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Broodmandje met boters & aioli</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>5.50</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Runde carpaccoi</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>6.50</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Garnalen avocado tartaar</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>6.95</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Geitenkaassalade (klein)</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>6.50</p>
-                    </div>
-                <div class="col-xs-12 col-md-12">
-                    <h2>Lekker bij de borrel of als voorgerechtje</h2>
-                </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Broodmandje met boters & aioli</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>5.50</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Runde carpaccoi</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>6.50</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Garnalen avocado tartaar</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>6.95</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Geitenkaassalade (klein)</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>6.50</p>
-                    </div>
-                <div class="col-xs-12 col-md-12">
-                    <h2>Maaltijdsalades</h2>
-                </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Zalm salade</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>12.50</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Tomaat met ricotta salade</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>11.50</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Geitenkaas salade</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>11.50</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Quiche van prei en gorgonzola</h3>
-                       <p>(Warm of koud)</p>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>12.50</p>
-                    </div>
+            <?php
+            // Soupen
+                    echo '<div class="col-xs-8 col-md-8">
+                                <h2>'. $data[1]["Categorie"].'<h2>
+                            </div>
+                            ';
+                    for($i = 1; $i <= $data[0]; $i++){
+                        if ($data[$i]['Categorie'] == 'Soepen & Brood'){
+                       echo '
+                            <div class="col-xs-8 col-md-8">
+                                <h3>'. $data[$i]["Naam"].'<h3>
+                            </div>
+                            
+                            <div class="col-xs-4 col-md-4 prijs">
+                                 <p>€'. $data[$i]["Prijs"] .'</p>
+                             </div>
+                             <div class="col-xs-12 col-md-12">
+                                 <p>'. $data[$i]["Beschrijving"].'<p>
+                            </div>
+                             ';
+                         }
+                     }
+            
+            // Tosti's
+                    echo '<div class="col-xs-8 col-md-8">
+                                <h2>Tosti\'s<h2>
+                            </div>
+                            ';
+                         for($i = 1; $i <= $data[0]; $i++){
+                            if ($data[$i]['Categorie'] == 'Tosti\'s'){
+                            echo '
+                            <div class="col-xs-8 col-md-8">
+                                <h3>'. $data[$i]["Naam"].'<h3>
+                            </div>
+                            
+                            <div class="col-xs-4 col-md-4 prijs">
+                                 <p>€'. $data[$i]["Prijs"] .'</p>
+                             </div>
+                             <div class="col-xs-12 col-md-12">
+                                 <p>'. $data[$i]["Beschrijving"].'<p>
+                            </div>
+                             ';
+                         }
+                    } 
+            
+            // Belegde broodjes
+                    echo '<div class="col-xs-8 col-md-8">
+                                <h2>Voorgerechten<h2>
+                            </div>
+                            ';
+                    for($i = 1; $i <= $data[0]; $i++){
+                        if ($data[$i]['Categorie'] == 'Belegde broodjes(italiaanse bol / stokbroodje / waldcorn)'){
+                       echo '
+                            <div class="col-xs-8 col-md-8">
+                                <h3>'. $data[$i]["Naam"].'<h3>
+                            </div>
+                            
+                            <div class="col-xs-4 col-md-4 prijs">
+                                 <p>€'. $data[$i]["Prijs"] .'</p>
+                             </div>
+                             <div class="col-xs-12 col-md-12">
+                                 <p>'. $data[$i]["Beschrijving"].'<p>
+                            </div>
+                             ';
+                         }
+                     }
+                
+
+                ?>
+            </div>
+            <div class="col-md-6">
+                <?php
+                // uitsmijters
+                    echo '<div class="col-xs-8 col-md-8">
+                                <h2>Uitsmijters<h2>
+                            </div>
+                            ';
+                    for($i = 1; $i <= $data[0]; $i++){
+                        if ($data[$i]['Categorie'] == 'uitsmijters'){
+                       echo '
+                            <div class="col-xs-8 col-md-8">
+                                <h3>'. $data[$i]["Naam"].'<h3>
+                            </div>
+                            
+                            <div class="col-xs-4 col-md-4 prijs">
+                                 <p>€'. $data[$i]["Prijs"] .'</p>
+                             </div>
+                             <div class="col-xs-12 col-md-12">
+                                 <p>'. $data[$i]["Beschrijving"].'<p>
+                            </div>
+                             ';
+                         }
+                     }
+                // Warme lunch
+                    echo '<div class="col-xs-8 col-md-8">
+                                <h2>Warme Lunch<h2>
+                            </div>
+                            ';
+                    for($i = 1; $i <= $data[0]; $i++){
+                        if ($data[$i]['Categorie'] == 'Warme lunch'){
+                       echo '
+                            <div class="col-xs-8 col-md-8">
+                                <h3>'. $data[$i]["Naam"].'<h3>
+                            </div>
+                            
+                            <div class="col-xs-4 col-md-4 prijs">
+                                 <p>€'. $data[$i]["Prijs"] .'</p>
+                             </div>
+                             <div class="col-xs-12 col-md-12">
+                                 <p>'. $data[$i]["Beschrijving"].'<p>
+                            </div>
+                             ';
+                         }
+                     }
+                // Salades
+                    echo '<div class="col-xs-8 col-md-8">
+                                <h2>Salades<h2>
+                            </div>
+                            ';
+                    for($i = 1; $i <= $data[0]; $i++){
+                        if ($data[$i]['Categorie'] == 'Salades'){
+                       echo '
+                            <div class="col-xs-8 col-md-8">
+                                <h3>'. $data[$i]["Naam"].'<h3>
+                            </div>
+                            
+                            <div class="col-xs-4 col-md-4 prijs">
+                                 <p>€'. $data[$i]["Prijs"] .'</p>
+                             </div>
+                             <div class="col-xs-12 col-md-12">
+                                 <p>'. $data[$i]["Beschrijving"].'<p>
+                            </div>
+                             ';
+                         }
+                     }
+                ?>
             </div>
 
-            <div class="col-xs-12 col-md-6">
-                <div class="col-xs-12 col-md-12">
-                    <h2>Hoofdgerechten - vlees</h2>
-                </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Lamsrack van de grill</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>17.95</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Gegrilde entrecote met mosterd en gegrilde tomaten</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>18.95</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Gegrilde kalsmedaillon</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>17.95</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Varkenshaas met gemarineerde champignons</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>15.95</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Mixed grill</h3>
-                       <p>(Varkenshaas medailllon, kup, lamsrack, minihamburgers van puur rundvlees</p>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>19.95</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Malse spareribs met zoete of pikante marinade</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>15.75</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Kip in het pannetje</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>14.75</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Kaasfondue</h3>
-                       <p>(Geserveerd met di erse soorten groeten)</p>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>15.75</p>
-                    </div>
-                <div class="col-xs-12 col-md-12">
-                    <h2>Hoofdgerechten - vis</h2>
-                </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Gamba's met brood en aioli</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>17.95</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Zeebaars met pesto</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>15.75</p>
-                    </div>
-                    <div class="col-xs-8 col-md-8">
-                       <h3>Gegratineerde scholrelletjes met paprika</h3>
-                    </div>
-                    <div class="col-xs-4 col-md-4 prijs">
-                        <p>15.95</p>
-                    </div>
+            <!-- Voorgerechten -->
+
+            <!-- Lekker voor bij de borrel -->
+
+            <!-- Maaltijdsalades -->
+
+                
+        <!-- Hoofdgerechten - vlees -->
+
+            <!-- Hoofdgerecht - vis -->
+
             </div>
         </div>
     </div>
