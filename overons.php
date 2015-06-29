af312017c22b5036d9e9d40c998b4d02b73fc843
mickey <mickeyberg401@gmail.com>
2015-06-29 13:59:29 +0200

    fix van de overons pagina

--- overons.php
+++ overons.php
@@ -20,55 +20,57 @@
                        </ol>
     </div>
 </div>
-<div class="content container geschiedenis">
-    <div class="border" style="box-shadow: 0px 0px 10px 0px black;">
-        <div class="col-md-6" style="float:left; padding: 10px 50px">
-            <div class="col-md-12" style="padding: 0px;">
-                <h2>Parkeren</h2>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate pulvinar turpis. consectetur adipiscing elit. Mauris vulputate pulvinar turpis. Suspendisse vel sem risus. 
-            </div>
-            
-            <div class="col-md-12" style="padding: 0px;">
-                <h2>Speeltuin</h2>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate pulvinar turpis. Suspendisse vel sem risus. consectetur adipiscing elit. Mauris vulputate pulvinar turpis. Suspendisse vel sem risus. consectetur adipiscing elit. Mauris vulputate pulvinar turpis. Suspendisse vel sem risus. 
-            </div>
-        </div>
-        <div class="col-md-6 hidden-xs" style="float:right">
-            <div class="text-overons" style="padding: 10px 50px">  
-                <div id="myCarousel" class="carousel slide" data-interval="5000" >
-                    <script type="text/javascript">
-                        $(document).ready(function(){
-                            $('.carousel').carousel();
-                        });
-                    </script>
-                        <div class="carousel-inner" style="width: 400px; height 300px; float:right;">
-                            <div class="item active">
-                                <img src="img/lunch.jpg" width="400px" height="300px" class="img-responsive"/>
-                            </div>
-                            <div class="item">
-                                <img src="img/locatie.jpg" width="400px" height="300px" class="img-responsive"/>
-                            </div>
-                            <div class="item">
-                                <img src="img/menu1.jpg" width="400px" height="300px" class="img-responsive"/>
-                            </div>
+<div class="content container" style="margin-bottom: 5px; padding-bottom:10px;">
+        <div class="border" style="border:1px solid black;box-shadow: 0px 0px 10px 0px black; overflow: hidden;background-color: #FFDCAA;">
+            <div class="col-md-12" style="margin: 0px; padding: 0 !important; margin: 0 !important; background-color: #FFDCAA;">
+                <div class="col-md-6" style="float:left; padding: 10px 50px">
+                    <div class="col-md-12" style="padding: 0px;">
+                        <h1>Parkeren</h1>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate pulvinar turpis. consectetur adipiscing elit. Mauris vulputate pulvinar turpis. Suspendisse vel sem risus. 
+                    </div>
+                    <div class="col-md-12" style="padding: 0px;">
+                        <h2>Speeltuin</h2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate pulvinar turpis. Suspendisse vel sem risus. consectetur adipiscing elit. Mauris vulputate pulvinar turpis. Suspendisse vel sem risus. consectetur adipiscing elit. Mauris vulputate pulvinar turpis. Suspendisse vel sem risus. 
+                    </div>
+                </div> 
+                <div class="col-md-6 hidden-xs" style="float:right">
+                    <div class="text-overons" style="padding: 10px 50px">  
+                        <div id="myCarousel" class="carousel slide" data-interval="5000" >
+                            <script type="text/javascript">
+                                $(document).ready(function(){
+                                    $('.carousel').carousel();
+                                });
+                            </script>
+                                <div class="carousel-inner" style="width: 400px; height 300px; float:right;">
+                                    <div class="item active">
+                                        <img src="img/lunch.jpg" width="400px" height="300px" class="img-responsive"/>
+                                    </div>
+                                    <div class="item">
+                                        <img src="img/locatie.jpg" width="400px" height="300px" class="img-responsive"/>
+                                    </div>
+                                    <div class="item">
+                                        <img src="img/menu1.jpg" width="400px" height="300px" class="img-responsive"/>
+                                    </div>
+                                </div>
+                            </div>        
                         </div>
-                    </div>        
-                </div>
-            </div>          
-        </div>
-        <div class="col-md-12" style="margin: 0px; padding: 0 !important; margin: 0 !important;">
+                    </div>          
+
+
+    </div>
+        <div class="col-md-12" style="margin: 0px; background-color: #FFDCAA; padding: 0 !important;margin-bottom: 5px;">
+
             <div class="text-geschiedenis" style="padding: 10px 50px">
-                <h2>Geschiedenis</h2>
+                <h1>Geschiedenis</h1>
                     <img src="img/locatie.jpg" width="255px" height="191px" class="img-responsive" style="float:right;" />            
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis dui vulputate diam cursus congue. Proin ligula arcu, consectetur ac auctor eget, fringilla sed nisi. Pellentesque id rutrum lectus. Mauris sollicitudin ligula id ipsum ultricies, ac tempor justo dapibus. Donec nulla dolor, tempus vitae aliquam auctor, aliquam et metus. Curabitur eleifend ultrices consequat. Mauris blandit, eros sit amet lacinia porta, purus orci dapibus odio, cursus tempus libero orci vel magna. Sed odio nulla, dictum vitae risus et, vestibulum porta mauris. Suspendisse in nisl elementum, lobortis turpis nec, vulputate lectus. Proin nec dolor nec dolor rhoncus eleifend eu a nibh. Proin et diam euismod, laoreet dolor congue, imperdiet nibh. Fusce euismod vulputate libero, sed dictum nisl lobortis vel. Fusce pulvinar vulputate auctor. Cras eleifend tellus non suscipit dapibus. Aliquam erat volutpat. Duis id volutpat felis.
                                 
                 </p>
             </div>
-    </div> 
-
-</div>
-
+            
+        </div>
+    </div>
 </div>
 
 
