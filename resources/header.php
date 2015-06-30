a96669936f088111c4bd6c2c1dfb2ba6c2c2b95a
JohJohan <johan.vlaar.1994@gmail.com>
2015-06-30 21:48:10 +0200

    fix active voor base links niet voor kaart

--- resources/header.php
+++ resources/header.php
@@ -1,3 +1,53 @@
+<?php 
+@session_start(); 
+
+    $index          =   "link";
+    $OverOns          =   "link";
+    $Reserveren          =   "link";
+    $Nieuws          =   "link";
+    $Gastenboek          =   "link";
+    $Contact          =   "link";
+    $Kaart          =   "link";
+    $KaartLunch          =   "link";
+    $KaartDiner         =   "link";
+    $KaartDessert         =   "link";
+
+    $menuLinks=basename($_SERVER['PHP_SELF'],".php");
+
+    switch ($menuLinks) {
+        case 'index':
+            $index              = "linkActive";
+            break;
+        case 'overons':
+            $OverOns              = "linkActive";
+            break;
+        case 'reserveren':
+            $Reserveren              = "linkActive";
+            break;
+        case 'nieuws':
+            $Nieuws              = "linkActive";
+            break;
+        case 'gastenboek':
+            $Gastenboek              = "linkActive";
+            break;
+        case 'contact':
+            $Contact              = "linkActive";
+            break;
+        case 'kaart/index':
+            $Kaart              = "linkActive";
+            break;
+        case 'kaart/lunch':
+            $KaartLunch              = "linkActive";
+            break;
+        case 'kaart/diner':
+            $KaartLunch              = "linkActive";
+            break;
+        case 'kaart/dessert':
+            $KaartLunch              = "linkActive";
+            break;
+    }
+
+?>
 <div class="container">
     <div class="masthead">
         <nav class="navbar-default navbar" role="navigation">
@@ -8,21 +58,21 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class=" active navbar-brand <?php echo $home; ?>" href="index.php" >Home</a>
+                <a class="active navbar-brand <?php echo $index; ?>" href="index.php" >Home</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a class="<?php echo $overons?> "       href="http://caferestauranthetwittepaard.nl/overons.php">Over ons</a></li>
-                    <li><a class="<?php echo $reserveren ?> "     href="http://caferestauranthetwittepaard.nl/reserveren.php">Reserveren</a></li>
-                    <li><a class="<?php echo $nieuws ?> "         href="http://caferestauranthetwittepaard.nl/nieuws.php">Nieuws</a></li>
-                    <li><a class="<?php echo $gastenboek ?> "     href="http://caferestauranthetwittepaard.nl/gastenboek.php">Gastenboek</a></li>
-                    <li><a class="<?php echo $contact ?> "        href="http://caferestauranthetwittepaard.nl/contact.php">Contact</a></li>
+                    <li><a class="<?php echo $OverOns ?> "       href="http://caferestauranthetwittepaard.nl/overons.php">Over ons</a></li>
+                    <li><a class="<?php echo $Reserveren ?> "     href="http://caferestauranthetwittepaard.nl/reserveren.php">Reserveren</a></li>
+                    <li><a class="<?php echo $Nieuws ?> "         href="http://caferestauranthetwittepaard.nl/nieuws.php">Nieuws</a></li>
+                    <li><a class="<?php echo $Gastenboek  ?> "     href="http://caferestauranthetwittepaard.nl/gastenboek.php">Gastenboek</a></li>
+                    <li><a class="<?php echo $Contact ?> "        href="http://caferestauranthetwittepaard.nl/contact.php">Contact</a></li>
                     <li class="dropdown">
-                        <a class="<?php echo $menu ?>" href="kaart/index.php" id="dropdownmenu">Onze kaart <b class="caret"></b></a>
+                        <a class="<?php echo $Kaart ?>" href="kaart/index.php" id="dropdownmenu">Onze kaart <b class="caret"></b></a>
                         <ul id="dropdownmenus" class="dropdown-menu">
-                            <li><a class="<?php echo $lunch ?> " id="dropdownmenu" href="http://caferestauranthetwittepaard.nl/kaart/lunch.php">Lunch</a></li>
-                            <li><a class="<?php echo $diner ?> " id="dropdownmenu" href="http://caferestauranthetwittepaard.nl/kaart/diner.php">Diner</a></li>
-                            <li><a class="<?php echo $desert ?> " id="dropdownmenu" href="http://caferestauranthetwittepaard.nl/kaart/dessert.php">Dessert</a></li>
+                            <li><a class="<?php echo $KaartLunch ?> " id="dropdownmenu" href="http://caferestauranthetwittepaard.nl/kaart/lunch.php">Lunch</a></li>
+                            <li><a class="<?php echo $KaartDiner ?> " id="dropdownmenu" href="http://caferestauranthetwittepaard.nl/kaart/diner.php">Diner</a></li>
+                            <li><a class="<?php echo $KaartDessert ?> " id="dropdownmenu" href="http://caferestauranthetwittepaard.nl/kaart/dessert.php">Dessert</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -46,54 +96,3 @@
             });
     });
 </script>
-
-<?php
-@session_start();
-    $home          =   "link";
-    $menu          =   "link";
-    $reserveren    =   "link";
-    $overons       =   "link";
-    $nieuws        =   "link";
-    $contact       =   "link";
-    $gastenboek    =   "link";
-
-
-
-    $menuLinks=basename($_SERVER['PHP_SELF'],".php");
-
-    switch ($menuLinks) {
-        case 'index':
-            $home               = "linkActiveHome";
-            break;
-        case 'menu':
-            $menu               = "linkActive";
-            break;
-        case 'gastenboek':
-            $gastenboek         = "linkActive";
-            break;
-        case 'reserveren':
-            $reserveren         = "linkActive";
-            break;
-        case 'overons':
-            $overons            = "linkActive";
-            break;
-        case 'nieuws':
-            $nieuws             = "linkActive";
-            break;
-        case 'kaart':
-            $kaart              = "linkActive";
-            break;
-        case 'lunch':
-            $lunch              = "linkActive";
-            break;
-        case 'diner':
-            $diner              = "linkActive";
-            break;
-        case 'dessert':
-            $dessert            = "linkActive";
-            break;
-        case 'contact':
-            $contact            = "linkActive";
-    }
-
-?>
