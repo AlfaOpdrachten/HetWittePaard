fb4274af59e3c0940e6f50a15e498385d2d17514
JohJohan <johan.vlaar.1994@gmail.com>
2015-07-01 10:42:38 +0200

    local files

--- resources/head.php
+++ resources/head.php
@@ -6,17 +6,17 @@
     <link rel="shortcut icon" href="img/favicon.ico">
 
     <!-- dit is  voor de kalender -->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+    <link rel="stylesheet" href="http://caferestauranthetwittepaard.nl/libs/jquery-ui.css">
+    <script src="http://caferestauranthetwittepaard.nl/libs/jquery-ui.js"></script>
+    <script src="http://caferestauranthetwittepaard.nl/libs/jquery-1.11.0.min.js"></script>
+    <script src="http://caferestauranthetwittepaard.nl/libs/jquery.validate.min.js"></script>
 
 
 	<!-- Bootstrap CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://caferestauranthetwittepaard.nl/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Bootstrap JavaScript -->
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+   <script src="http://caferestauranthetwittepaard.nl/libs/bootstrap.min.js"></script>
     
     <!-- Custom CSS -->
 	<link rel="stylesheet" href="http://caferestauranthetwittepaard.nl/css/main.css">
