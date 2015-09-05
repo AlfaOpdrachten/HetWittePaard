f6864176610ddb8e78da16043347ea5498293645
KIPdeKIP <kipdekip@gmail.com>
2015-09-05 12:10:18 +0200

    Los favicon problemen op Fixes #1.

--- resources/head.php
+++ resources/head.php
@@ -3,7 +3,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="http://caferestauranthetwittepaard.nl/favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?0">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?0">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?0">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?0">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?0">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?0">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?0">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?0">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?0">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png?0" sizes="32x32">
+    <link rel="icon" type="image/png" href="/android-chrome-192x192.png?0" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png?0" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png?0" sizes="16x16">
+    <link rel="manifest" href="/manifest.json?0">
+    <link rel="shortcut icon" href="/favicon.ico?0">
+    <meta name="msapplication-TileColor" content="#1f4333">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png?0">
+    <meta name="theme-color" content="#ffffff">
     <!-- dit is  voor de kalender -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
